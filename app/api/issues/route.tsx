523a19d303c0d4d1b130f2f57410cf340022b4c2
kartikpavan <kartik.varanasi@yahoo.com>
2023-10-13 01:20:48 +0530

    Build an API for creating new issues

---
+++ app/api/issues/route.tsx
@@ -0,0 +1,30 @@
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+import prisma from "@/prisma/client";
+
+const createIssueSchema = z.object({
+  title: z.string().min(1).max(255),
+  description: z.string().min(1).max(255),
+});
+
+export async function POST(request: NextRequest) {
+  const body = await request.json(); // getting the body
+  //   ZOD validation
+  const validation = createIssueSchema.safeParse(body);
+  // unsuccesful validation
+  if (!validation.success) {
+    return NextResponse.json(
+      { error: validation.error.errors },
+      { status: 400 }
+    );
+  }
+  // if data is valid store in DB
+  const newIssue = await prisma.issue.create({
+    data: {
+      title: body.title,
+      description: body.description,
+    },
+  });
+
+  return NextResponse.json(newIssue, { status: 201 });
+}
