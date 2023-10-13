0607e67ffc630d82b891b4b11f8d9ed6c388773c
kartikpavan <kartik.varanasi@yahoo.com>
2023-10-13 19:37:46 +0530

    Created a custom LinkComponent using nextLink and RadixLink

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,10 +1,10 @@
 import prisma from "@/prisma/client";
-import { Button, Table } from "@radix-ui/themes";
+import { Table } from "@radix-ui/themes";
 import React from "react";
 import IssueStatusBadge from "../components/IssueStatusBadge";
 import delay from "delay";
 import IssuesToolbar from "./IssuesToolbar";
-import Link from "next/link";
+import CustomLink from "../components/CustomLink";
 
 const IssuesPage = async () => {
   const issues = await prisma.issue.findMany();
@@ -30,8 +30,10 @@ const IssuesPage = async () => {
             return (
               <Table.Row key={issue.id}>
                 <Table.Cell>
-                  <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
-                  <div className="block sm:hidden">
+                  <CustomLink href={`/issues/${issue.id}`}>
+                    {issue.title}
+                  </CustomLink>
+                  <div className="block sm:hidden mt-1">
                     <IssueStatusBadge status={issue.status} />
                   </div>
                 </Table.Cell>
