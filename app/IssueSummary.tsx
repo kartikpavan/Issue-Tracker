11ee9b1e249cdd61f3139b834e8ce880abf491f0
kartikpavan <kartik.varanasi@yahoo.com>
2023-10-18 15:57:02 +0530

    Issue Summary Component with issues count

--- app/IssueSummary.tsx
+++ app/IssueSummary.tsx
@@ -1,7 +1,35 @@
-import { Flex } from "@radix-ui/themes";
+import prisma from "@/prisma/client";
+import { Box, Card, Flex, Heading, Text } from "@radix-ui/themes";
+import { PropsWithChildren } from "react";
 
-const IssueSummary = () => {
-  return <Flex></Flex>;
+const IssueSummary = async () => {
+  const open = await prisma.issue.count({ where: { status: "OPEN" } });
+  const closed = await prisma.issue.count({ where: { status: "CLOSED" } });
+  const inProgress = await prisma.issue.count({
+    where: { status: "IN_PROGRESS" },
+  });
+  return (
+    <Flex gap="5" mt="3">
+      <div className="bg-red-100/20 p-4 rounded-md border-2 border-red-400/60 shadow-sm">
+        <Heading size="3">Open Issues</Heading>
+        <Text className="font-semibold" color="red">
+          {open}
+        </Text>
+      </div>
+      <div className="bg-violet-100/20 p-4 rounded-md border-2 border-violet-400/60 shadow-sm">
+        <Heading size="3">In-Progress Issues</Heading>
+        <Text className="font-semibold" color="violet">
+          {open}
+        </Text>
+      </div>
+      <div className="bg-green-100/20 p-4 rounded-md border-2 border-green-400/60 shadow-sm">
+        <Heading size="3">Closed Issues</Heading>
+        <Text className="font-semibold" color="green">
+          {closed}
+        </Text>
+      </div>
+    </Flex>
+  );
 };
 
 export default IssueSummary;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import IssueSummary from "./IssueSummary";
 import LatestIssues from "./LatestIssues";
 
 export default function Home() {
   return (
     <main>
       <LatestIssues />
+      <IssueSummary />
     </main>
   );
 }
